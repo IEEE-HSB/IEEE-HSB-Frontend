07ed378898ba6d2a1fbc01d9f651d51a635bf289
cheetah-10 <mennashehata2005@gmail.com>
2025-12-14 11:30:37 +0200

    edit gallery type

--- src/types/gallery.ts
+++ src/types/gallery.ts
@@ -7,4 +7,4 @@ export type GalleryType = {
   chapterId?: string,
 }
 
-export type ChapterFilter = 'IEEE' | 'WIE' | 'CS' | 'PES' | 'RAS' | 'COMSOC';
+export type ChapterFilter = 'ALL' | 'WIE' | 'CS' | 'PES' | 'RAS' | 'COMSOC';

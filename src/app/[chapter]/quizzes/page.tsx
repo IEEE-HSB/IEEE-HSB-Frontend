1190da86606d18d4ef37f17d0611efb8de9eabf8
cheetah-10 <mennashehata2005@gmail.com>
2026-02-21 23:37:12 +0200

    edit dashboard folder place

--- src/app/[chapter]/quizzes/page.tsx
+++ src/app/[chapter]/quizzes/page.tsx
@@ -90,7 +90,7 @@ export default function QuizzesPage({ params }: QuizzesPageProps) {
                     color: `var(--${mainColor}-100)`,
                   }}
                 >
-                  {committee}
+                  {committee} 
                 </Link>
               </motion.li>
             ))}

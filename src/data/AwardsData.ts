e51ac67f86893f18555e8c61ba18c44f15c9dd8b
cheetah-10 <mennashehata2005@gmail.com>
2026-04-28 12:05:31 +0300

    fix: qoutaion error vercel

--- src/data/AwardsData.ts
+++ src/data/AwardsData.ts
@@ -2,7 +2,7 @@ export const awardsData = [
   {
     year: "2025",
     name: "Gold Darrel Chong Student Activity Award",
-    description: "Awarded for the outstanding organization and impact of the event 'Auxilio 7.0'."
+    description: "Awarded for the outstanding organization and impact of the event &quotAuxilio 7.0&quot."
   },
   {
     year: "2025",
